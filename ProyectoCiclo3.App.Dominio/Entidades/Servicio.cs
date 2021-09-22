2634fa0b05dc82dd27a2c90e19b97c951d2e0ec4
lucianeme <44451586+lucianeme@users.noreply.github.com>
2021-09-21 20:28:43 -0500

    FirstCommit

---
+++ ProyectoCiclo3.App.Dominio/Entidades/Servicio.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProyectoCiclo3.App.Dominio{
+
+    public class Servicio{
+        public int id { get; set; }
+        public int origen { get; set; }
+        public int destino { get; set;}
+        public string fecha { get; set; }
+        public int encomienda { get; set; }
+    }
+}
